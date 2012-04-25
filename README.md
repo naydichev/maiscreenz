753a4a1e22029e9d28158029b59be61f98cb8666
julian <rublind@gmail.com>
2012-04-25 12:58:06 -0700

    Add a Known Bugs section to README and add Growl Notifications.

--- README.md
+++ README.md
@@ -4,12 +4,20 @@ maiscreenz
 A simply python script to upload screenshots to your own server
 
 Requirements
-============
+------------
 
 - [MacFSEvents][0]: for the mac wonderfulness
 - [paramiko][1]: for ssh magicsauce
 - [xerox][2]: for clipboard amazingness
+- [gntp][3]: a protocol for growling at you
+
+Known Bugs
+----------
+
+- `CTRL + C` does not work, I've been forced to `CTRL + Z / kill -9 %1` for now
+- No install process, this is coming up, I promise
 
 [0]: http://pypi.python.org/pypi/MacFSEvents
 [1]: http://www.lag.net/paramiko
 [2]: https://github.com/kennethreitz/xerox
+[3]: https://github.com/kfdm/gntp/
--- maiscreenz.py
+++ maiscreenz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from fsevents import Stream,Observer
-import sys, os, hashlib, paramiko, xerox
+import sys, os, hashlib, paramiko, xerox, gntp.notifier
 
 #############
 # <configs> #
@@ -12,6 +12,7 @@ username = 'diaf'
 scp_path = 'html/'
 web_path = '/'
 file_str = 'Screen Shot'
+growl    = True
 ##############
 # </configs> #
 ##############
@@ -29,15 +30,25 @@ def hash_for_file(f):
 # 128 => move_to
 # 256 => create
 # 512 => delete
-def callback(event):
+def upload(event):
     # moved to a name with Screen Shot in it
-    if event.mask is 128 and file_str in str(event.name):
+    fileName = str(event.name)
+    if event.mask is 128 and file_str in fileName:
         # split the extension
         fileExtension = os.path.splitext(event.name)[1]
-        newName = str(hash_for_file(event.name))[0:6] + fileExtension
-        upload_file(event.name, scp_path + newName)
-        xerox.copy(protocol + hostname + web_path + newName)
-
+        hash_str = hash_for_file(fileName)
+        newName = hash_str[0:6] + fileExtension
+        upload_file(fileName, scp_path + newName)
+        web_url = protocol + hostname + web_path + newName
+        xerox.copy(web_url)
+        if growl:
+            growl.notify(
+                    title = "Upload Complete",
+                    description = "Uploaded %s to %s" % (fileName, web_url),
+                    noteType = "Upload Complete",
+                    sticky = False,
+                    priority = 1,
+                    )
 
 def upload_file(local_file, remote_file):
     client = paramiko.SSHClient()
@@ -49,14 +60,32 @@ def upload_file(local_file, remote_file):
     client.close()
     return ret
 
+def register_growl():
+    growl = gntp.notifier.GrowlNotifier(
+            applicationName = "maiscreenz",
+            notifications = [ "Upload Complete" ],
+            defaultNotifications = [ "Upload Complete" ],
+            )
+    growl.register()
+
+    return growl
+
+# usage check
 if len(sys.argv) < 2:
     print "Must specify a path"
     sys.exit(1)
 
+# setup runtime variables
 watchPath = sys.argv[1]
 
+# setup growl
+if growl:
+    growl = register_growl()
+
+# setup
 observer = Observer()
-stream = Stream(callback, watchPath, file_events=True)
+stream = Stream(upload, watchPath, file_events=True)
 observer.schedule(stream)
+
+# go go go
 observer.run()
-ssh_client.close()
